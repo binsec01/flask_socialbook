c08b9b343a833602b355d86334aa8e22a47ce86c
binsec01 <abusyed.official@gmail.com>
2025-05-01 12:10:30 +0600

    blocked whitespace

--- challenge/config.py
+++ challenge/config.py
@@ -5,5 +5,5 @@ class Config:
     BASE_DIR = os.path.dirname(__file__)
     UPLOAD_FOLDER = os.path.join(BASE_DIR, 'static', 'uploads')
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
-    BLOCKED_CHARACTERS = ['&', '|', ';', '`', '$', '(', ')', ' ', '\n', '\r', '\t','--']
+    BLOCKED_CHARACTERS = ['&', '|', ';', '`', '$', '(', ')', ' ', '\n', '\r', '\t','--', ' ','%20', '%0A', '%0D', '%3B', '%26', '%7C', '%60', '%24', '%28', '%29']
     MAX_CONTENT_LENGTH = 4 * 1024 * 1024 # 4MB max file size
